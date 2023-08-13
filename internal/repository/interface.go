441afca8c9f350a8d1efeb2c1e7d8268872beaa0
giffone <galimzhanov.faizulla@gmail.com>
2023-08-14 00:42:42 +0600

    storage - done task fix

--- internal/repository/interface.go
+++ internal/repository/interface.go
@@ -9,7 +9,7 @@ type Db interface {
 	CreateTask(ctx context.Context, t *domain.TaskDTO) error
 	UpdateTask(ctx context.Context, id string, t *domain.TaskDTO) error
 	DeleteTask(ctx context.Context, id string) error
-	DoneTask() error
+	DoneTask(ctx context.Context, id string) error
 	GetTasks() error
 }
 
