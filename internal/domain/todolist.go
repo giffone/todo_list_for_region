25a5319ba00462129d893b98ed62486e88a8921b
giffone <galimzhanov.faizulla@gmail.com>
2023-08-13 15:45:43 +0600

    add title model

--- internal/domain/todolist.go
+++ internal/domain/todolist.go
@@ -1,1 +1,22 @@
 package domain
+
+import (
+	"errors"
+	"time"
+	"unicode/utf8"
+)
+
+type List struct {
+	Title    string    `json:"title"`
+	ActiveAt time.Time `json:"activeAt"`
+}
+
+func (l *List) Validate() error {
+	if l.Title == "" {
+		return errors.New("Title must not be empty")
+	}
+	if utf8.RuneCountInString(l.Title) > 200 {
+		return errors.New("Title length must be maximum 200 symbols")
+	}
+	return nil
+}
