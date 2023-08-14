f8097b640a1bad22f01d3e2d8549c8aedbf283b0
academie_green02 <academie_green02@academie-green02s-iMac.local>
2023-08-14 16:19:48 +0600

    update graceful shutdown

--- internal/server/server.go
+++ internal/server/server.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"log"
 	"net/http"
-	"os"
 	"os/signal"
 	"syscall"
 	"time"
@@ -15,28 +14,20 @@ import (
 )
 
 type Server struct {
-	quit chan os.Signal
-	e    *echo.Echo
-	cfg  *config.ServerConf
-	env  *env
+	e   *echo.Echo
+	cfg *config.ServerConf
+	env *env
 }
 
-func New(h *api.Handlers) *Server {
-	srv := Server{
-		quit: make(chan os.Signal, 1),
-		e:    newRouter(h),
-	}
-	// register notifiers
-	signal.Notify(srv.quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
-	return &srv
-}
+func New(h *api.Handlers) *Server { return &Server{e: newRouter(h)} }
 
 func (s *Server) Run() {
-	// stop envs - mongodb etc.
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
 	defer func() {
 		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 		defer cancel()
-
+		// stop envs - mongodb etc
 		s.env.Stop(ctx)
 	}()
 
@@ -44,19 +35,19 @@ func (s *Server) Run() {
 	go func() {
 		if err := s.e.Start(s.cfg.Addr); err != nil && err != http.ErrServerClosed {
 			log.Printf("server start error: %s\n", err.Error())
-			s.quit <- syscall.SIGQUIT
+			cancel()
 		}
 	}()
 
-	// wait notifiers [ctrl-c]
-	<-s.quit
+	// wait system notifiers or cancel func
+	<-ctx.Done()
 	log.Println("stopping server")
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
+	ctx2, cancel2 := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel2()
 
 	// stop router
-	if err := s.e.Shutdown(ctx); err != nil {
+	if err := s.e.Shutdown(ctx2); err != nil {
 		log.Printf("server stopping error: %s\n", err.Error())
 		return
 	}
